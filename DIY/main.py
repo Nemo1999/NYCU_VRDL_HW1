74095d728297abbe11560662eb8efd417f535eda
BO YU, Cheng <nemo1999.eecs06@g2.nctu.edu.tw>
2021-11-02 21:14:24 +0800

    DIY

---
+++ DIY/main.py
@@ -0,0 +1,209 @@
+import os
+import tqdm
+import numpy
+import random
+import csv
+import torch
+import torch.utils.data as td
+import torchvision as tv
+import torch.nn.functional as F
+import torchvision.transforms.functional as TF
+import sklearn.model_selection as sksm
+
+import my_custom_dataset
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+OUT_DIR = './Results'
+RANDOM_SEED = 42
+
+random.seed(RANDOM_SEED)
+#os.makedirs = (OUT_DIR, exist_ok=True)
+
+
+def get_model_desc(pretrained=False, num_classes=200, use_attention=False):
+    """
+    Generates description string.
+    """
+    desc = list()
+
+    if pretrained:
+        desc.append('Transfer')
+    else:
+        desc.append('Baseline')
+
+    if num_classes == 204:
+        desc.append('Multitask')
+
+    if use_attention:
+        desc.append('Attention')
+
+    return '-'.join(desc)
+
+
+def log_accuracy(path_to_csv, desc, acc, sep='\t', newline='\n'):
+    """
+    Logs accuracy into a CSV-file.
+    """
+    file_exists = os.path.exists(path_to_csv)
+
+    mode = 'a'
+    if not file_exists:
+        mode += '+'
+
+    with open(path_to_csv, mode) as csv:
+        if not file_exists:
+            csv.write(f'setup{sep}accuracy{newline}')
+
+        csv.write(f'{desc}{sep}{acc}{newline}')
+
+
+
+# transform images
+
+def pad(img, fill=0, size_max=500):
+    """
+    Pads images to the specified size (height x width).
+    Fills up the padded area with value(s) passed to the `fill` parameter.
+    """
+    pad_height = max(0, size_max - img.height)
+    pad_width = max(0, size_max - img.width)
+
+    pad_top = pad_height // 2
+    pad_bottom = pad_height - pad_top
+    pad_left = pad_width // 2
+    pad_right = pad_width - pad_left
+
+    return TF.pad(img, (pad_left, pad_top, pad_right, pad_bottom), fill=fill)
+
+# fill padded area with ImageNet's mean pixel value converted to range [0, 255]
+fill = tuple(map(lambda x: int(round(x * 256)), (0.485, 0.456, 0.406)))
+# pad images to 500 pixels
+max_padding = tv.transforms.Lambda(lambda x: pad(x, fill=fill))
+
+transforms_train = tv.transforms.Compose([
+   max_padding,
+   tv.transforms.RandomOrder([
+       tv.transforms.RandomCrop((375, 375)),
+       tv.transforms.RandomHorizontalFlip(),
+       tv.transforms.RandomVerticalFlip()
+   ]),
+   tv.transforms.ToTensor(),
+   tv.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+transforms_eval = tv.transforms.Compose([
+   max_padding,
+   tv.transforms.CenterCrop((375, 375)),
+   tv.transforms.ToTensor(),
+   tv.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+
+# load images
+ds_train = my_custom_dataset.trainset(transform=transforms_train)
+ds_val = my_custom_dataset.testset(transform=transforms_train)
+ds_eval = my_custom_dataset.evalset(transform=transforms_eval)
+
+# set hyper-parameters
+params = {'batch_size': 24, 'num_workers': 8}
+num_epochs = 100
+num_classes = 200
+
+# instantiate data loaders
+train_loader = td.DataLoader(
+    dataset=ds_train,
+    shuffle=True,
+    **params
+)
+val_loader = td.DataLoader(
+    dataset=ds_val,
+    shuffle=True,
+    **params
+)
+test_loader = td.DataLoader(dataset=ds_eval, batch_size=1)
+
+# instantiate the model
+model = tv.models.resnet50(num_classes=num_classes).to(DEVICE)
+print(model)
+
+# instantiate optimizer and scheduler
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.95)
+
+
+# generate model description string
+model_desc = get_model_desc(num_classes=num_classes)
+
+# define the training loop
+best_snapshot_path = None
+val_acc_avg = list()
+best_val_acc = -1.0
+
+for epoch in range(num_epochs):
+
+    # train the model
+    model.train()
+    train_loss = list()
+    for batch in train_loader:
+        x, y = batch
+
+        x = x.to(DEVICE)
+        y = y.to(DEVICE)
+
+        optimizer.zero_grad()
+
+        # predict bird species
+        y_pred = model(x)
+
+        # calculate the loss
+        loss = F.cross_entropy(y_pred, y)
+
+        # backprop & update weights
+        loss.backward()
+        optimizer.step()
+
+        train_loss.append(loss.item())
+
+    # validate the model
+    model.eval()
+    val_loss = list()
+    val_acc = list()
+    with torch.no_grad():
+        for batch in val_loader:
+            x, y = batch
+
+            x = x.to(DEVICE)
+            y = y.to(DEVICE)
+
+            # predict bird species
+            y_pred = model(x)
+
+            # calculate the loss
+            loss = F.cross_entropy(y_pred, y)
+
+            # calculate the accuracy
+            acc = skms.accuracy_score([val.item() for val in y], [val.item() for val in y_pred.argmax(dim=-1)])
+
+            val_loss.append(loss.item())
+            val_acc.append(acc)
+
+        val_acc_avg.append(np.mean(val_acc))
+
+        # save the best model snapshot
+        current_val_acc = val_acc_avg[-1]
+        if current_val_acc > best_val_acc:
+            if best_snapshot_path is not None:
+                os.remove(best_snapshot_path)
+
+            best_val_acc = current_val_acc
+            best_snapshot_path = os.path.join(OUT_DIR, f'model_{model_desc}_ep={epoch}_acc={best_val_acc}.pt')
+
+            torch.save(model.state_dict(), best_snapshot_path)
+
+    # adjust the learning rate
+    scheduler.step()
+
+    # print performance metrics
+    if (epoch == 0) or ((epoch + 1) % 10 == 0):
+        print('Epoch {} |> Train. loss: {:.4f} | Val. loss: {:.4f} | Val. acc: {:.4f}'.format(
+            epoch + 1, np.mean(train_loss), np.mean(val_loss), np.mean(val_acc))
+        )
---
+++ DIY/my_custom_dataset.py
@@ -0,0 +1,77 @@
+from PIL import Image
+from os.path import join
+from torch.utils.data import Dataset
+
+def get_class_dicts(root):
+    class2int = dict()
+    int2class = dict()
+    with open(join(root, 'classes.txt')) as f :
+        lines = f.readlines()
+        for l in lines:
+            index , class_name = int(l.split('.')[0].strip()), l.strip()
+            class2int[class_name] = index
+            int2class[index] = class_name
+        return class2int, int2class
+
+class trainset(Dataset):
+    def __init__(self, transform=None, root='/home/nemo/VRDL2021/HW1_Fine_Grained/datasets/CUB'):
+        self.root = root
+        self.transform=transform
+        self.class2int, self.int2class = get_class_dicts(root)
+        self.labels = dict()
+        self.files = []
+        with open(join(root,'training_labels.txt')) as f:
+            for l in f.readlines():
+                file , label = l.split(' ')[0].strip(), int(l.split(' ')[1].split('.')[0].strip())
+                self.labels[file] = label
+                self.files.append(file)
+    def __len__(self):
+        return int(len(self.files)*0.9)
+    def __getitem__(self, idx):
+        label = self.labels[self.files[idx]]
+        img = Image.open(join(self.root,'training_images', self.files[idx]))
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+
+class testset(Dataset):
+    def __init__(self, transform=None, root='/home/nemo/VRDL2021/HW1_Fine_Grained/datasets/CUB'):
+        self.root = root
+        self.transform=transform
+        self.class2int, self.int2class = get_class_dicts(root)
+        self.labels = dict()
+        self.files = []
+        with open(join(root,'training_labels.txt')) as f:
+            for l in f.readlines():
+                file , label = l.split(' ')[0].strip(), int(l.split(' ')[1].split('.')[0].strip())
+                self.labels[file] = label
+                self.files.append(file)
+    def __len__(self):
+        # this line is different from class trainset
+        return len(self.files) - int(len(self.files)*0.9)
+    def __getitem__(self, idx):
+        # this line is different from class trainset
+        idx = idx + int(len(self.files)*0.9)
+        label = self.labels[self.files[idx]]
+        img = Image.open(join(self.root,'training_images', self.files[idx]))
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+
+class evalset(Dataset):
+    def __init__(self, transform=None, root='/home/nemo/VRDL2021/HW1_Fine_Grained/datasets/CUB'):
+        self.root = root
+        self.transform = transform
+        self.class2int, self.int2class = get_class_dicts(root)
+        self.files = []
+        with open(join(root,'testing_img_order.txt')) as f:
+            for file in f.readlines():
+                self.files.append(file.strip())
+        def __len__(self):
+            return len(self.files)
+        def __getitem__(self, index):
+            img = Image.open(join(self.rot,'testing_images',self.files[index]))
+            if self.transform:
+                img = self.transform(img)
+            return self.files[index], img
